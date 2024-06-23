842dcd4e56972dc80ec64dd3ab7e7e6b09df3bd1
Rameses Barria <ramesesluis@gmail.com>
2024-06-23 22:07:39 +0800

    updated methods at login.vue

--- resources/js/components/forms/login.vue
+++ resources/js/components/forms/login.vue
@@ -5,38 +5,43 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        items: null,
-        fields: ['id', 'USERNAME']
-      }
-    },
-    methods:{
-      get_accounts(){
-        axios.get('/get_accounts').then(response=>{
-          console.log(response.data);
-          this.items = response.data;
-        }).catch(error=>{
-          console.log(error.data);
-        })
-      }
-    },
+import axios from 'axios';
 
-    methods: {
-      register(){
-        axios.post('/register/registration', this.form).then(response=>{
-          console.log(response.data.email);
-          console.log(response.data.password);
-        }).catch(error=>{
-          console.log(error.data);
-          this.loading=false;
-        })
-      }
+export default {
+  data() {
+    return {
+      items: null,
+      fields: ['id', 'USERNAME'],
+      form: {
+        email: '',
+        password: ''
+      },
+      loading: false
+    };
+  },
+  methods: {
+    get_accounts() {
+      axios.get('/get_accounts').then(response => {
+        console.log(response.data);
+        this.items = response.data;
+      }).catch(error => {
+        console.log(error);
+      });
     },
-
-    beforeMount(){
-      this.get_accounts();
+    register() {
+      this.loading = true;
+      axios.post('/register/registration', this.form).then(response => {
+        console.log(response.data.email);
+        console.log(response.data.password);
+        this.loading = false;
+      }).catch(error => {
+        console.log(error);
+        this.loading = false;
+      });
     }
+  },
+  beforeMount() {
+    this.get_accounts();
   }
+};
 </script>
